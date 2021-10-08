45abe82e3ca293ac847afe9ff5c7a51743a82c1d
Mario Van Steenberghe <mario.vansteenberghe@gmail.com>
2021-10-08 20:19:38 +0200

    runnables with lambda's

--- src/main/java/be/flexlineitsolutions/udemy/java8/lambdas/RunnableLambdaExample.java
+++ src/main/java/be/flexlineitsolutions/udemy/java8/lambdas/RunnableLambdaExample.java
@@ -1,4 +1,38 @@
 package be.flexlineitsolutions.udemy.java8.lambdas;
 
 public class RunnableLambdaExample {
+
+	public static void main(String[] args) {
+
+		// prior to java 8
+
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Running 1");
+			}
+		};
+
+		new Thread(runnable).start();
+
+		// java 8 lambda
+		// () -> {};
+
+		Runnable runnableLambda = () -> {
+			System.out.println("Inside runnable 2");
+		};
+
+		new Thread(runnableLambda).start();
+
+		// or:
+
+		Runnable runnableLambda2 = () -> System.out.println("Inside runnable 2");
+		new Thread(runnableLambda2).start();
+
+		// or:
+
+		new Thread(() -> System.out.println("Inside runnable 2")).start();
+
+	}
+
 }
